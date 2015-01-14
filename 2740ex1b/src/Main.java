83d0fd9f173b498fc8b89fe0d0158536a6072732
Mark Swanson <mswanson@southeastmn.edu>
2015-01-14 16:30:33 -0600

    Initial commit

---
+++ 2740ex1b/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+        System.out.println("p123 #1:");
+
+        System.out.println("\np123 #3:");
+
+        System.out.println("\np124 #5:");
+
+        System.out.println("\np124 #6:");
+	}
+
+}
